aae33cd90b8ed16b6059fb3b3062976cbee8d0fb
Ivan Plaza <100290861@alumnos.uc3m.es>
2023-07-07 17:25:56 +0200

    Add springBoot starter dependecies

---
+++ src/main/java/com/ivan/authentication/proxy/AuthenticationProxyApplication.java
@@ -0,0 +1,13 @@
+package com.ivan.authentication.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationProxyApplication.class, args);
+	}
+
+}
